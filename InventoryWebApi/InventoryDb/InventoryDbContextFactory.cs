411b85db97a322337ad454cbdbb1705f8161ddcd
zero <khhily@163.com>
2024-12-10 23:13:48 +0800

    initial project

---
+++ InventoryWebApi/InventoryDb/InventoryDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace InventoryDb;
+
+public class InventoryDbContextFactory : IDesignTimeDbContextFactory<InventoryDbContext>
+{
+    public InventoryDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<InventoryDbContext>();
+        var connectionString = "Server=124.71.97.110;Database=MedicineInventory;uid=inventory;pwd=JeK1ohI&lfeVDS;";
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+        return new InventoryDbContext(optionsBuilder.Options);
+    }
+}
