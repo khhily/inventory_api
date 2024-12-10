54ab39ed0a514c89ab454cb80c4ce3157baf059b
zero <khhily@163.com>
2024-12-10 23:04:59 +0800

    init project

--- InventoryDb/InventoryDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryData.Entity;
-using Microsoft.EntityFrameworkCore;
-
-namespace InventoryDb;
-
-public class InventoryDbContext(DbContextOptions<InventoryDbContext> options) : DbContext(options)
-{
-    public DbSet<Inventory> Inventory { get; set; } = default!;
-}
